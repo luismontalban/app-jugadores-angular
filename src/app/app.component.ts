4c77997338660f0cc1074528e55ead7ea3c35bbc
Luis <luis.montalbanv@gmail.com>
2022-11-20 01:05:39 -0300

    Todo OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Jugador } from './models/jugador';
+import { Component } from '@angular/core'
+import { Jugador } from './models/jugador'
 
 @Component({
   selector: 'app-root',
@@ -7,20 +7,24 @@ import { Jugador } from './models/jugador';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  titulo = 'Listado de Jugadores';
-  jugadores: Jugador[] = [new Jugador('Cristiano Ronaldo', 'Manchester United', 'Portugal', 7), new Jugador('Lionel Messi', 'PSG', 'Argentina', 10)];
+  titulo = 'Listado de Jugadores'
+  jugadores: Jugador[] = [
+    new Jugador('Cristiano Ronaldo', 'Manchester United', 'Portugal', 7),
+    new Jugador('Lionel Messi', 'PSG', 'Argentina', 10)
+  ]
 
-  nombreInput: string = '';
-  equipoInput: string = '';
-  paisInput: string = '';
-  dorsalInput: number = 0;
+  nombreInput: string = ''
+  equipoInput: string = ''
+  paisInput: string = ''
+  dorsalInput: number = 0
 
- agregarJugador(){
-    let jugador1 = new Jugador(this.nombreInput, this.equipoInput, this.paisInput, this.dorsalInput);
-    this.jugadores.push(jugador1);
+  agregarJugador () {
+    let jugador1 = new Jugador(
+      this.nombreInput,
+      this.equipoInput,
+      this.paisInput,
+      this.dorsalInput
+    )
+    this.jugadores.push(jugador1)
   }
-
-
-
-
 }
